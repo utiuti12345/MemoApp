6b8a5c243a93e23d2c1c338f1168c5c8e73556a3
Yuta Saotome <42575295+utiuti12345@users.noreply.github.com>
2023-02-26 13:20:56 +0900

    Initial Commit

---
+++ MemoApp Watch App/MemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoAppApp.swift
+//  MemoApp Watch App
+//
+//  Created by saotome on 2023/02/26.
+//
+
+import SwiftUI
+
+@main
+struct MemoApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemoApp/MemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoAppApp.swift
+//  MemoApp
+//
+//  Created by saotome on 2023/02/26.
+//
+
+import SwiftUI
+
+@main
+struct MemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
